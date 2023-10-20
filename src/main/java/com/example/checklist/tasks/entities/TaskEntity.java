b1a3b312d3711616516e8cd312f0f42a59752f55
Nicolas <nvegass01@gmail.com>
2023-10-20 20:56:27 +0100

    feat(data structure): Initial data structure project.

---
+++ src/main/java/com/example/checklist/tasks/entities/TaskEntity.java
@@ -0,0 +1,25 @@
+package com.example.checklist.tasks.entities;
+import lombok.*;
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "task")
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_task", nullable = false)
+    private long idTask;
+
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
+
+    @Column(name = "description", length = 100, nullable = false)
+    private String description;
+
+    @Column(name = "status", length = 10, nullable = false)
+    private String status;
+}
