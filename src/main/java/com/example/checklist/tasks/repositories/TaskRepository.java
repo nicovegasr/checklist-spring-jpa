79ec6a433e0e8f067567be66b20237468cc73120
Nicolas <nvegass01@gmail.com>
2023-10-20 21:10:44 +0100

    feat(data repository): JPA repository enabled.

---
+++ src/main/java/com/example/checklist/tasks/repositories/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.example.checklist.tasks.repositories;
+
+import com.example.checklist.tasks.entities.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+}
